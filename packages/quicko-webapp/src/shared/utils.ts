9e7bfdc913c9ebe85ce89f76bc8c85d8199866e1
DanielPopOut <stephane.tchangang@gmail.com>
2020-06-03 14:12:58 +0200

    Improvement quicko.json : stringify state directly
    Aim is to be able to understand quicko.json updates on peer reviews

--- packages/quicko-webapp/src/shared/utils.ts
+++ packages/quicko-webapp/src/shared/utils.ts
@@ -8,23 +8,11 @@ export const bumpStyled = (component: any) => (css: TemplateStringsArray) =>
     }
   `;
 
-export const convertStoreToFileText = (store: RootState) => {
-    const text = Object.keys(store).reduce(
-      (acc: any, key: string) =>
-      // @ts-ignore next-line
-        ({...acc, [key]: JSON.stringify(store[key], null, 2)}),
-      {}
-    );
-
-    return JSON.stringify(text, null, 2)
+export const convertStoreToFileText = (state: RootState) => {
+    return JSON.stringify(state, null, 2)
 }
 
-export const convertFileTextToStore = (text: string) => {
-  let quickoData = JSON.parse(text);
-  quickoData = {
-    playbooks: JSON.parse(quickoData.playbooks),
-    tasks: JSON.parse(quickoData.tasks),
-  };
-
+export const convertFileTextToStore = (stateText: string) => {
+  let quickoData = JSON.parse(stateText);
   return quickoData;
 }
